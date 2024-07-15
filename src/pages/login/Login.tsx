1a5a770433fd5c752338f55c58e83c3a0a75471e
José Ferreira <josebazjunior09@gmail.com>
2024-07-15 15:56:47 -0300

    [feat]: improve login form

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,60 +1,12 @@
-import React, { useState } from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  faUser,
-  faLock,
-  faEye,
-  faEyeSlash,
-} from "@fortawesome/free-solid-svg-icons";
+import React from "react";
 import "./Login.css";
+import LoginForm from "./LoginForm";
 
 const Login: React.FC = () => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [showPassword, setShowPassword] = useState(false);
-
-  const handleSubmit = (e: React.FormEvent) => {
-    e.preventDefault();
-  };
-
-  const togglePasswordVisibility = () => {
-    setShowPassword(!showPassword);
-  };
-
   return (
     <div className="login-container">
       <div className="login-form-container">
-        <form className="login-form" onSubmit={handleSubmit}>
-          <h2>AcompanhaHealth</h2>
-          <label>
-            <div className="login-icon">
-              <FontAwesomeIcon icon={faUser} />
-            </div>
-            <input
-              placeholder="Email"
-              type="email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              required
-            />
-          </label>
-          <label>
-            <div className="login-icon">
-              <FontAwesomeIcon icon={faLock} />
-            </div>
-            <input
-              placeholder="Senha"
-              type={showPassword ? "text" : "password"}
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              required
-            />
-            <div className="login-icon" onClick={togglePasswordVisibility}>
-              <FontAwesomeIcon icon={showPassword ? faEyeSlash : faEye} />
-            </div>
-          </label>
-          <button type="submit">Entrar</button>
-        </form>
+        <LoginForm/>
       </div>
       <div className="login-img-container">
         <img src="src/assets/login-background-img.jpg" alt="" />
---
+++ src/pages/login/LoginForm.tsx
@@ -0,0 +1,117 @@
+import { Button, Checkbox, Form, Grid, Input, theme, Typography } from "antd";
+import { LockOutlined, MailOutlined } from "@ant-design/icons";
+
+const { useToken } = theme;
+const { useBreakpoint } = Grid;
+const { Text, Title, Link } = Typography;
+
+export default function LoginForm() {
+  const { token } = useToken();
+  const screens = useBreakpoint();
+
+  const onFinish = (values: any) => {
+    console.log("Received values of form: ", values);
+  };
+
+  const styles = {
+    container: {
+      margin: "0 auto",
+      padding: screens.md
+        ? `${token.paddingXL}px`
+        : `${token.sizeXXL}px ${token.padding}px`,
+      width: "380px",
+    },
+    forgotPassword: {
+      float: "right" as "right" | "left" | "none" | "initial" | "inherit",
+    },
+    header: {
+      marginBottom: token.marginSM,
+    },
+    logo: {
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    section: {
+      alignItems: "center",
+      backgroundColor: token.colorBgContainer,
+      display: "flex",
+    },
+    text: {
+      color: token.colorTextSecondary,
+    },
+    title: {
+      fontSize: screens.md ? token.fontSizeHeading2 : token.fontSizeHeading3,
+    },
+  };
+
+  return (
+    <section style={styles.section}>
+      <div style={styles.container}>
+        <div style={styles.header}>
+          <div style={styles.logo}>
+            <Title style={styles.title}>AcompanhaHealth - Login</Title>
+          </div>
+          <Text style={styles.text}>
+            Bem-vindo de volta ao AcompanhaHealth! Por favor, insira seus dados
+            abaixo para fazer login.
+          </Text>
+        </div>
+        <Form
+          name="normal_login"
+          initialValues={{
+            remember: true,
+          }}
+          onFinish={onFinish}
+          layout="vertical"
+          requiredMark="optional"
+        >
+          <Form.Item
+            name="email"
+            rules={[
+              {
+                type: "email",
+                required: true,
+                message: "Por favor insira seu e-mail!",
+              },
+            ]}
+          >
+            <Input prefix={<MailOutlined />} placeholder="Email" />
+          </Form.Item>
+          <Form.Item
+            name="password"
+            rules={[
+              {
+                required: true,
+                message: "Por favor insira sua senha!",
+              },
+            ]}
+          >
+            <Input.Password
+              prefix={<LockOutlined />}
+              type="password"
+              placeholder="Senha"
+            />
+          </Form.Item>
+          <Form.Item>
+            <Form.Item name="remember" valuePropName="checked" noStyle>
+              <Checkbox>Lembrar de mim</Checkbox>
+            </Form.Item>
+            <a style={styles.forgotPassword} href="">
+              Esqueceu a senha?
+            </a>
+          </Form.Item>
+          <Form.Item style={{ marginBottom: "0px" }}>
+            <Button block={true} type="primary" htmlType="submit">
+              Entrar
+            </Button>
+            <div>
+              <Text style={styles.text}>Não possui uma conta?</Text>{" "}
+              <Link href="">Solicite acesso</Link>
+            </div>
+          </Form.Item>
+        </Form>
+      </div>
+    </section>
+  );
+}
