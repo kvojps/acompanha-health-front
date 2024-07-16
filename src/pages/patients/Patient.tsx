5b9156b98b9a545c920a4f6c7b3ea3597539ca6b
José Ferreira <josebazjunior09@gmail.com>
2024-07-16 00:34:53 -0300

    [feat]: add patient table

--- src/pages/patients/Patient.tsx
+++ src/pages/patients/Patient.tsx
@@ -1,10 +1,55 @@
 import React from "react";
 import LayoutSchema from "../../components/layout/Layout";
+import dataSource from "./PatientMockData";
+import { Table} from "antd";
 
 const Patient: React.FC = () => {
+  const columns = [
+    {
+      title: "Nome",
+      dataIndex: "name",
+      key: "name",
+    },
+    {
+      title: "CNS",
+      dataIndex: "cns",
+      key: "cns",
+    },
+    {
+      title: "Data de nascimento",
+      dataIndex: "birthDate",
+      key: "birthDate",
+    },
+    {
+      title: "Gênero",
+      dataIndex: "gender",
+      key: "gender",
+    },
+    {
+      title: "Estágio da gravidez",
+      dataIndex: "pregnancyStage",
+      key: "pregnancyStage",
+    },
+    {
+      title: "Raça",
+      dataIndex: "race",
+      key: "race",
+    },
+    {
+      title: "Nível de educação",
+      dataIndex: "educationLevel",
+      key: "educationLevel",
+    },
+    {
+      title: "Nome da mãe",
+      dataIndex: "motherName",
+      key: "motherName",
+    }
+  ]
+
   return (
     <LayoutSchema>
-      <h1>Patient</h1>
+      <Table dataSource={dataSource} columns={columns} />
     </LayoutSchema>
   );
 };
